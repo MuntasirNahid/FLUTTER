ebff27d3b3a0ffa2b2b88667b5f76495743be86e
MuntasirNahid <muntasir19@student.sust.edu>
2023-12-20 14:30:22 +0600

    new commit

---
+++ login_form_using_bloc/lib/login_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+import 'package:login_form_using_bloc/widgets/gradient_button.dart';
+import 'package:login_form_using_bloc/widgets/login_field.dart';
+import 'package:login_form_using_bloc/widgets/social_button.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset('assets/images/signin_balls.png'),
+              const Text(
+                'Sign in.',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50,
+                ),
+              ),
+              const SizedBox(height: 50),
+              const SocialButton(
+                  iconPath: 'assets/images/google.png',
+                  label: 'Continue with Google'),
+              const SizedBox(height: 20),
+              const SocialButton(
+                iconPath: 'assets/images/fb.png',
+                label: 'Continue with Facebook',
+                horizontalPadding: 90,
+              ),
+              const SizedBox(height: 15),
+              const Text(
+                'or',
+                style: TextStyle(
+                  fontSize: 17,
+                ),
+              ),
+              const SizedBox(height: 15),
+              LoginField(
+                hintText: 'Email',
+                controller: emailController,
+              ),
+              const SizedBox(height: 15),
+              LoginField(
+                hintText: 'Password',
+                controller: passwordController,
+              ),
+              const SizedBox(height: 20),
+              const GradientButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
