e82a094ea15731af98aabb251b0d3ee816f6d8ad
MuntasirNahid <muntasir19@student.sust.edu>
2024-01-19 09:54:18 +0600

    Feat: Add Storing Post Data in Firebase

--- instagram_clone/lib/resources/storage_methods.dart
+++ instagram_clone/lib/resources/storage_methods.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:uuid/uuid.dart';
 
 class StorageMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -14,6 +15,12 @@ class StorageMethods {
     //like profilePic/uid/
     Reference ref =
         _storage.ref().child(childName).child(_auth.currentUser!.uid);
+
+    if (isPost) {
+      String id = const Uuid().v1();
+      //means we are creating an unique post id under ref directory
+      ref = ref.child(id);
+    }
 
     //Ready the file want to upload
     UploadTask uploadTask = ref.putData(file); //using putData for Uint8List
--- instagram_clone/lib/screens/add_post_screen.dart
+++ instagram_clone/lib/screens/add_post_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:instagram_clone/models/user.dart';
 import 'package:instagram_clone/providers/user_provider.dart';
+import 'package:instagram_clone/resources/firestore_methods.dart';
 import 'package:instagram_clone/utills/colors.dart';
 import 'package:instagram_clone/utills/utills.dart';
 import 'package:provider/provider.dart';
@@ -17,8 +18,10 @@ class AddPostScreen extends StatefulWidget {
 
 class _AddPostScreenState extends State<AddPostScreen> {
   Uint8List? _file;
+  bool isLoading = false;
 
   final TextEditingController _descriptionController = TextEditingController();
+
   _selectImage(BuildContext context) async {
     return showDialog(
         context: context,
@@ -64,6 +67,53 @@ class _AddPostScreenState extends State<AddPostScreen> {
         });
   }
 
+  void postImage(
+    String uid,
+    String username,
+    String profImage,
+  ) async {
+    try {
+      setState(() {
+        isLoading = true;
+      });
+      String res = await FirestoreMethods().uploadPost(
+        _descriptionController.text,
+        _file!,
+        uid,
+        username,
+        profImage,
+      );
+
+      if (res == "success") {
+        setState(() {
+          isLoading = false;
+        });
+        showSnackBar('Posted!', context);
+        clearImage();
+      } else {
+        setState(() {
+          isLoading = false;
+        });
+        showSnackBar(res.toString(), context);
+      }
+    } catch (err) {
+      showSnackBar(err.toString(), context);
+    }
+  }
+
+  void clearImage() {
+    setState(() {
+      _file = null;
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _descriptionController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final UserModel user = Provider.of<UserProvider>(context).getUser;
@@ -79,13 +129,17 @@ class _AddPostScreenState extends State<AddPostScreen> {
               backgroundColor: mobileBackgroundColor,
               leading: IconButton(
                 icon: const Icon(Icons.arrow_back),
-                onPressed: () {},
+                onPressed: clearImage,
               ),
               title: const Text('Post To'),
               centerTitle: false,
               actions: [
                 TextButton(
-                  onPressed: () {},
+                  onPressed: () => postImage(
+                    user.uid,
+                    user.username,
+                    user.photoUrl,
+                  ),
                   child: const Text(
                     'Post',
                     style: TextStyle(
@@ -99,6 +153,12 @@ class _AddPostScreenState extends State<AddPostScreen> {
             ),
             body: Column(
               children: [
+                isLoading
+                    ? const LinearProgressIndicator()
+                    : const Padding(
+                        padding: EdgeInsets.only(top: 0),
+                      ),
+                const Divider(),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   crossAxisAlignment: CrossAxisAlignment.start,
