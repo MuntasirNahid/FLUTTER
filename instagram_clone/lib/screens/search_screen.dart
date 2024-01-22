6dd803226ed5dc64654364fe77c8ab2332676ed5
MuntasirNahid <muntasir19@student.sust.edu>
2024-01-22 19:30:41 +0600

    ProfilePageUi and Functionality Done

--- instagram_clone/lib/screens/search_screen.dart
+++ instagram_clone/lib/screens/search_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:instagram_clone/screens/profile_screen.dart';
 import 'package:instagram_clone/utills/colors.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -52,14 +53,27 @@ class _SearchScreenState extends State<SearchScreen> {
                 return ListView.builder(
                     itemCount: (snapshot.data! as dynamic).docs.length,
                     itemBuilder: (context, index) {
-                      return ListTile(
-                        leading: CircleAvatar(
-                          backgroundImage: NetworkImage(
-                            (snapshot.data! as dynamic).docs[index]['photoUrl'],
+                      return InkWell(
+                        onTap: () => Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return ProfileScreen(
+                                uid: (snapshot.data! as dynamic).docs[index]
+                                    ['uid'],
+                              );
+                            },
                           ),
                         ),
-                        title: (snapshot.data! as dynamic).docs[index]
-                            ['username'],
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                              (snapshot.data! as dynamic).docs[index]
+                                  ['photoUrl'],
+                            ),
+                          ),
+                          title: (snapshot.data! as dynamic).docs[index]
+                              ['username'],
+                        ),
                       );
                     });
               })
