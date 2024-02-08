635135779a48f10dcde7d7f7a058de661c55ee07
MuntasirNahid <69505101+MuntasirNahid@users.noreply.github.com>
2024-02-08 15:17:44 +0600

    Delete clean_code_architecture directory

--- clean_code_architecture/android/app/src/main/kotlin/com/example/clean_code_architecture/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.clean_code_architecture
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- clean_code_architecture/lib/features/daily_news/domain/repository/article_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:clean_code_architecture/core/resources/data_state.dart';
-import 'package:clean_code_architecture/features/daily_news/domain/entities/article.dart';
-
-abstract class ArticleRepository {
-  Future<DataState<List<ArticleEntity>>> getNewsArticles();
-}
